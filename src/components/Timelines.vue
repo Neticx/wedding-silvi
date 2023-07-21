dd16b3283da19ee3387d6d1ad04687dff95fe956
Fahmi Ulul Azmi <fahmi@moladin.com>
2023-07-21 08:49:36 +0700

    fix: typo and maps

--- src/components/Timelines.vue
+++ src/components/Timelines.vue
@@ -19,7 +19,7 @@
               <decoration class="w-2/12 mx-auto my-2 fill-white"></decoration>
               <p class="text-white">{{ timeline.location }}</p>
               <p class="text-gray-200">{{ timeline.address }}</p>
-              <a class="btn" href="">
+              <a class="btn" href="https://goo.gl/maps/xNhgoQb9uYTEjVmi7">
                 <i class="fa-solid fa-map"></i>
                 Lihat Peta
               </a>
